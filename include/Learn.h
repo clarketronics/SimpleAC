163822f09385812afad358aea09041fc8d42c50a
clarketronics <rhys.clarke@clarketronics.co.uk>
2021-08-21 20:26:48 +0100

    Bit of tidy up removing folders etc.

--- include/Learn.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Arduino.h"
-#include "FlashBeep.h"
-#include "Data.h"
-#include "Config.h"
-#include "Helpers.h"
-#include "Sleep.h"
-
-void learn(Data &data, NFCReader &nfcReader, FlashBeep &feedback, Helpers &helpers);
